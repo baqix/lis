e150d7f50d1e5964e897704a6004142bf6a9f5c4
baqix <gszloda@gmail.com>
2024-10-22 09:30:49 +0200

    napraw sie

--- listta.js
+++ listta.js
@@ -1,2 +1,30 @@
 console.log();
 console.log('Numer Zadania');
+console.log('----------------');
+console.log();
+let lista_kontaktow= [
+    {
+        name: "Maxwell Wright",
+        phone: "(0191) 719 6495",
+        email: "Curabitur.egestas.nunc@nonummyac.co.uk",
+    },
+    {
+        name: "Raja Villarreal",
+        phone: "0866 398 2895",
+        email: "posuere.vulputate@sed.com",
+    },
+    {
+        name: "Helen Richards",
+        phone: "0800 1111",
+        email: "libero@convallis.edu",
+    }
+]
+let imie=lista_kontaktow[0].name;
+let imie=lista_kontaktow[1].name;
+let imie=lista_kontaktow[2].name;//imie
+let imie=lista_kontaktow[0].phone;
+let imie=lista_kontaktow[1].phone;
+let imie=lista_kontaktow[2].phone;//tel
+let imie=lista_kontaktow[0].email;
+let imie=lista_kontaktow[1].email;
+let imie=lista_kontaktow[2].email;//email
